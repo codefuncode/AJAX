da51b6cc075c21de7e3434cfc42dde13b6f21c39
codefuncode <codefuncode@protonmail.com>
2020-05-20 13:17:09 -0400

    Comntarios en el AJAX

--- Prueba/js.js
+++ Prueba/js.js
@@ -5,19 +5,31 @@ var edad = document.getElementById('edad');
 // enviar.addEventListener('click', showHint('Carlos', 38), false);
 
 enviar.onclick = function() {
+    // Llamamos al a función que enviara los datos  y le pasamos los parámetros que necesita la función 
 
     showHint(nombre.value, edad.value);
 };
 
 function showHint(x, y) {
-
+    //  instancia del objeto 
     var xmlhttp = new XMLHttpRequest();
+    // Evento que indica que el estado a cambiado 
     xmlhttp.onreadystatechange = function() {
+
+        //  Condicional que indica exito de la transacción 
+
         if (this.readyState == 4 && this.status == 200) {
 
+            // Para trabajar Con Json debemos  convertir el método responseTesxt a objeto Json 
             var mydata = JSON.parse(this.responseText);
+
+            //  Los datos que vienen del servidor alojados en la variable se 
+            //  escribirnos el nombre de la variable seguido de un punto  y en nombre del valor 
+            // típico de matrices asociativas 
+
             // console.log(this.responseText.name);
             // console.log(this.responseText.age);
+
             console.log(mydata.name);
             console.log(mydata.age);
 
@@ -26,6 +38,11 @@ function showHint(x, y) {
 
         }
     };
+
+    //  Estos métodos pertenecientes al objeto son los responsables de enviar los datos  
+    // y declarar el formato de envió  y por ultimo enviarlos, este es el orden
+    // correcto de la declaración  para su funcionamiento adecuado. 
+
     xmlhttp.open("GET", "php.php?nombre=" + x + "&edad=" + y, true);
     xmlhttp.setRequestHeader("Content-type", "json");
     xmlhttp.send();
